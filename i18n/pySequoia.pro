f56bfc6fb92f481f4b8268e9564e57ac83379ce6
aroche <augustin.roche@gmail.com>
2013-09-21 17:13:00 +0200

    using Qt Linguist for translation

---
+++ i18n/pySequoia.pro
@@ -0,0 +1,6 @@
+SOURCES = ../pySequoia.py \
+    ../tree.py
+FORMS = ../ui/pySequoia.ui \
+    ../ui/select_indiv_dialog.ui \
+    ../ui/options.ui
+TRANSLATIONS = pySequoia_fr.ts
---
+++ i18n/pySequoia_fr.ts
@@ -0,0 +1,261 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../tree.py" line="157"/>
+        <source>from</source>
+        <translation type="unfinished">à partir de</translation>
+    </message>
+    <message>
+        <location filename="../tree.py" line="164"/>
+        <source>File saved</source>
+        <translation type="unfinished">Fichier enregistré</translation>
+    </message>
+    <message>
+        <location filename="../tree.py" line="354"/>
+        <source>go</source>
+        <translation type="unfinished">lien</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="14"/>
+        <source>pySequoia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="27"/>
+        <source>Gedcom file</source>
+        <translation type="unfinished">Fichier Gedcom</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="234"/>
+        <source>Browse...</source>
+        <translation type="unfinished">Sélectionner...</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="48"/>
+        <source>Tree type</source>
+        <translation type="unfinished">Type d&apos;arbre</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="60"/>
+        <source>Ascending</source>
+        <translation type="unfinished">Ascendant</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="76"/>
+        <source>Descending</source>
+        <translation type="unfinished">Descendant</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="84"/>
+        <source>Orientation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="90"/>
+        <source>Portrait</source>
+        <translation type="unfinished">Portrait</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="100"/>
+        <source>Landscape</source>
+        <translation type="unfinished">Paysage</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="112"/>
+        <source>Encoding</source>
+        <translation type="unfinished">Encodage</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="120"/>
+        <source>UTF-8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="125"/>
+        <source>Latin1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="136"/>
+        <source>Reload file</source>
+        <translation type="unfinished">Recharger le fichier</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="151"/>
+        <source>Include  images</source>
+        <translation type="unfinished">Inclure les images</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="161"/>
+        <source>Create  index</source>
+        <translation type="unfinished">Créer un index</translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="185"/>
+        <source>Number of generations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="224"/>
+        <source>Save PDF as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="243"/>
+        <source>Base individual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="272"/>
+        <source>Select</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="304"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/pySequoia.ui" line="314"/>
+        <source>Préférences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pySequoia.ui" line="319"/>
+        <source>A propos de pySequoia...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Options</name>
+    <message>
+        <location filename="../ui/options.ui" line="17"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="28"/>
+        <source>Image size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="57"/>
+        <source>Complementary individual information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="69"/>
+        <source>Birth/death dates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="85"/>
+        <source>Occupation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="101"/>
+        <source>Notes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="115"/>
+        <source>Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="151"/>
+        <source>Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="162"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="167"/>
+        <source>6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="172"/>
+        <source>8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="177"/>
+        <source>10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="182"/>
+        <source>11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="187"/>
+        <source>12</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="192"/>
+        <source>14</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="197"/>
+        <source>16</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="202"/>
+        <source>18</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="207"/>
+        <source>20</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="220"/>
+        <source>Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="228"/>
+        <source>Women</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../ui/options.ui" line="1101"/>
+        <source>Sélectionner...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/options.ui" line="676"/>
+        <source>Men</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>select_indiv_dialog</name>
+    <message>
+        <location filename="../ui/select_indiv_dialog.ui" line="14"/>
+        <source>Select base individual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/select_indiv_dialog.ui" line="25"/>
+        <source>Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
